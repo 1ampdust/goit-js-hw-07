cb5d56574c3c7f63fecfb1b67bd80e4a4b63b8e2
Lampdust <lampdust06@gmail.com>
2023-11-25 17:28:20 +0200

    upd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// 01-gallery.js
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+let activeInstance = null;
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                  <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>
+              </li>`;
+    })
+    .join("");
+}
+
+function renderGallery() {
+  const galleryMarkup = createGalleryMarkup(galleryItems);
+  galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  renderGallery();
+
+  galleryContainer.addEventListener("click", function (event) {
+    event.preventDefault();
+    const galleryLink = event.target.closest(".gallery__link");
+
+    if (galleryLink) {
+      const imageUrl = galleryLink.getAttribute("href");
+
+      if (activeInstance) {
+        activeInstance.close();
+      }
+
+      activeInstance = basicLightbox.create(`
+        <img src="${imageUrl}" alt="Image">
+      `);
+
+      activeInstance.show();
+    }
+  });
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape" && activeInstance) {
+      activeInstance.close();
+    }
+  });
+});
